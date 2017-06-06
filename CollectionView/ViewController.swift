80bdeda84370d6d483cf8cadbcc346e859741b08
Olga Klyushkina <klyushkina.olga@gmail.com>
2017-06-06 14:43:07 +0300

    Fixed the bug regarding opening a wrong image in the second view controller.

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UICollectionViewController {
     var currentImageN = 0
     var myImageArray = [UIImage]()
     var myIndex = IndexPath()
+   // var myIndex = Int()
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,86 +32,107 @@ class ViewController: UICollectionViewController {
             return myImageArray.count
     }
     
+
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
         cell.image.image = myImageArray[indexPath.row]
-        myIndex = indexPath
+        //myIndex = indexPath.row
         return cell
     }
     
     @IBAction func addButtonAction(_ sender: UIBarButtonItem) {
-        self.navigationItem.prompt = ""
+        self.navigationItem.prompt = nil
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         alertController.addAction(UIAlertAction(title: "load 3 in parallels", style: .default, handler: { action in
             // load images in parallers
             // code
             let session = URLSession(configuration: .ephemeral)
             for _ in 0...2 {
-                let url = URL(string: links[self.currentImageN])
-                self.currentImageN += 1
-                let task = session.dataTask(with: url!) {(data, responce, error) in
-                    DispatchQueue.main.async {
-                        if data != nil {
-                            self.myImageArray.append(UIImage(data: data!)!)
-                            self.collectionView?.reloadData()
+                if self.currentImageN <= links.count {
+                    let url = URL(string: links[self.currentImageN])
+                    self.currentImageN += 1
+                    let task = session.dataTask(with: url!) {(data, responce, error) in
+                        DispatchQueue.main.async {
+                            if data != nil {
+                                self.myImageArray.append(UIImage(data: data!)!)
+                                self.collectionView?.reloadData()
+                            }
                         }
                     }
-                }
                 task.resume()
+                }
             }
         }))
         alertController.addAction(UIAlertAction(title: "load 3 image by image", style: .default, handler: { (action) in
             // load 3 image by image
             // code
             let session = URLSession(configuration: .ephemeral)
-            var url = URL(string: links[self.currentImageN])
-            self.currentImageN += 1
-            let task1 = session.dataTask(with: url!) {(data, responce, error) in
-                DispatchQueue.main.async {
-                    if data != nil {
-                        self.myImageArray.append(UIImage(data: data!)!)
-                        self.collectionView?.reloadData()
-                        self.currentImageN += 1
-                            }
-                        }
-                    }
-            task1.resume()
-            url = URL(string: links[self.currentImageN + 1])
-            let task2 = session.dataTask(with: url!) {(data, responce, error) in
+            if self.currentImageN <= links.count {
+                var url = URL(string: links[self.currentImageN])
+                self.currentImageN += 1
+                let task1 = session.dataTask(with: url!) {(data, responce, error) in
                     DispatchQueue.main.async {
                         if data != nil {
                             self.myImageArray.append(UIImage(data: data!)!)
                             self.collectionView?.reloadData()
                             self.currentImageN += 1
+                            }
+                        if self.currentImageN + 1 <= links.count {
+                            url = URL(string: links[self.currentImageN + 1])
+                            let task2 = session.dataTask(with: url!) {(data, responce, error) in
+                                DispatchQueue.main.async {
+                                    if data != nil {
+                                        self.myImageArray.append(UIImage(data: data!)!)
+                                        self.collectionView?.reloadData()
+                                        self.currentImageN += 1
+                                }
+                            }
+                            if self.currentImageN + 2 <= links.count {
+                                url = URL(string: links[self.currentImageN + 2])
+                                let task3 = session.dataTask(with: url!) {(data, responce, error) in
+                                    DispatchQueue.main.async {
+                                        if data != nil {
+                                            self.myImageArray.append(UIImage(data: data!)!)
+
+                                            self.collectionView?.reloadData()
+                                            self.currentImageN += 1
+                                            }
+                                        }
+                                    }
+                            task3.resume()
+                                }
+                            }
+                    task2.resume()
                         }
                     }
                 }
-            task2.resume()
-            url = URL(string: links[self.currentImageN + 2])
-            let task3 = session.dataTask(with: url!) {(data, responce, error) in
-                DispatchQueue.main.async {
-                    if data != nil {
-                        self.myImageArray.append(UIImage(data: data!)!)
-                        self.collectionView?.reloadData()
-                        self.currentImageN += 1
-                    }
-                }
+            task1.resume()
             }
-            task3.resume()
         }))
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alertController, animated: true, completion: nil)
         }
-
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let ImageVC = segue.destination as? ImageVC {
+        if let myIndex = getIndexPathForSelectedCell() {
+            if let ImageVC = segue.destination as? ImageVC {
                 ImageVC.bigPhoto = self.myImageArray[myIndex.row]
+            }
         }
     }
     
+    func getIndexPathForSelectedCell() -> NSIndexPath? {
+        var myIndex: NSIndexPath?
+        
+        if (collectionView?.indexPathsForSelectedItems?.count)! > 0 {
+            myIndex = collectionView?.indexPathsForSelectedItems?[0] as NSIndexPath?
+        }
+        return myIndex
+    }
     
+
     override func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
     }
